1b37154cac69bb6b4c8d10d262e489fb8d67c11a
Monika <os_mo@wp.pl>
2024-05-31 22:48:57 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import HeroCard from '@/components/HeroCard.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HeroCard />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HeroCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    That's a hero card
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
